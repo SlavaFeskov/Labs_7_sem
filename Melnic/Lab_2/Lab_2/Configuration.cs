04fb3d1e8088fdb6df7b6c84eb0c65162a3cdb66
Viachaslau Fiaskou <feskov.vyacheslav@gmail.com>
2018-09-15 15:15:15 +0300

    gaussian distribution was implemented

--- Melnic/Lab_2/Lab_2/Configuration.cs
+++ Melnic/Lab_2/Lab_2/Configuration.cs
@@ -3,13 +3,15 @@ using System.Configuration;
 
 namespace Lab_2
 {
-    public class Configuration
+    public static class Configuration
     {
-        public long GetR0 => Int64.Parse(GetValue("r0"));
-        public long GetA => Int64.Parse(GetValue("a"));
-        public long GetM => Int64.Parse(GetValue("m"));
+        public static long GetR0 => Int64.Parse(GetValue("r0"));
+        public static long GetA => Int64.Parse(GetValue("a"));
+        public static long GetM => Int64.Parse(GetValue("m"));
+        public static long GetAmount => Int64.Parse(GetValue("amountOfValues"));
+        public static long GetAmountOfSequences => Int64.Parse(GetValue("amountOfSequences"));
 
-        public string GetValue(string name)
+        private static string GetValue(string name)
         {            
             return ConfigurationManager.AppSettings[name];
         }
