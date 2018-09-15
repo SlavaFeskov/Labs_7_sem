d869046c2af689970b8f0942fa9cee9e9ef037cf
Viachaslau Fiaskou <feskov.vyacheslav@gmail.com>
2018-09-15 15:15:36 +0300

    even distribution was changed

--- Melnic/Lab_2/Lab_2/EvenDistribution.cs
+++ Melnic/Lab_2/Lab_2/EvenDistribution.cs
@@ -5,11 +5,9 @@ namespace Lab_2
 {
     public static class EvenDistribution
     {
-        public static List<double> GenerateValues(double a, double b, int amount)
-        {            
-            var config = new Configuration();            
-            var rrValues = Generator.GenerateValues(config.GetR0, amount, config.GetA, config.GetM);
-            return rrValues.Select(el => a + (b - a) * el).ToList();
+        public static List<double> GenerateValues(double a, double b)
+        {                    
+            return Data.GetRrSequence().Select(el => a + (b - a) * el).ToList();
         }
     }
 }
