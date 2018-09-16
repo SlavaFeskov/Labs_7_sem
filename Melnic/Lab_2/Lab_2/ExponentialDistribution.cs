b41258b8a7e80506680aad4fcce98106b2881307
Viachaslau Fiaskou <feskov.vyacheslav@gmail.com>
2018-09-16 15:03:22 +0300

    exponential, gamma, triangle distributions were added

---
+++ Melnic/Lab_2/Lab_2/ExponentialDistribution.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab_2
+{
+    public static class ExponentialDistribution
+    {
+        public static List<double> GetValues(double lambda)
+        {
+            return Data.GetRrSequence().Select(el => -1 / lambda * Math.Log(el, Math.E)).ToList();
+        }
+    }
+}
---
+++ Melnic/Lab_2/Lab_2/TriangleDistribution.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Windows.Documents;
+
+namespace Lab_2
+{
+     public class TriangleDistribution : IDistributional
+    {
+        public List<double> GetValues(params object[] parameters)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public AnalysisModel GetMathAttributes(List<double> values)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
