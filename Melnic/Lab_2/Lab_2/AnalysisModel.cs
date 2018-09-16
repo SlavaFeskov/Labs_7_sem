a81726efe18a10a8ba0034fa35e77f3f3a8f31d7
Viachaslau Fiaskou <feskov.vyacheslav@gmail.com>
2018-09-16 15:02:54 +0300

    new architecture was implemented

---
+++ Melnic/Lab_2/Lab_2/AnalysisModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Lab_2
+{
+    public class AnalysisModel
+    {
+        public double MathExpectation { get; set; }
+        public double Dispersion { get; set; }
+        public double Sigma { get; set; }
+
+        public AnalysisModel()
+        {             
+        }
+
+        public AnalysisModel(double mathExpectation, double dispersion, double sigma)
+        {
+            MathExpectation = mathExpectation;
+            Dispersion = dispersion;
+            Sigma = sigma;
+        }
+    }
+}
---
+++ Melnic/Lab_2/Lab_2/IDistributional.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Windows.Documents;
+
+namespace Lab_2
+{
+    public interface IDistributional
+    {
+        List<double> GetValues(params object[] parameters);
+        AnalysisModel GetMathAttributes(List<double> values);
+    }
+}
