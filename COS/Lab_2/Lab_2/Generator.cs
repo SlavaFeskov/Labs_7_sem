bd61cb96663c530da12f8657954da745974be899
Viachaslau Fiaskou <feskov.vyacheslav@gmail.com>
2018-09-17 10:46:02 +0300

    lab_2 cos initial commit

---
+++ COS/Lab_2/Lab_2/Generator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Lab_2
+{
+    public class Generator
+    {
+        public List<List<double>> GenerateAllSequences(int K, int N)
+        {
+            var harmonicFunc = new HarmonicFunction();
+            var result = new List<List<double>>();
+            for (int M = K; M <= 2 * N; M++)
+            {
+                result.Add(harmonicFunc.GetValues(M, N, 0));
+            }
+
+            return result;
+        }
+    }
+}
---
+++ COS/Lab_2/Lab_2/HarmonicFunction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab_2
+{
+    public class HarmonicFunction
+    {
+        public List<double> GetValues(int M, int N, double fi)
+        {
+            var result = new List<double>();
+            for (int n = 0; n < M; n++)
+            {
+                result.Add(Math.Sin(2 * Math.PI * n / N + fi));
+            }
+            return result;
+        }
+    }
+}
