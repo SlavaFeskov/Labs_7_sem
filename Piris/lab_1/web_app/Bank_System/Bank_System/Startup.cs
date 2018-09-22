bce698715da4fb0d8c44049288f518ac5b2c7ca2
Viachaslau Fiaskou <feskov.vyacheslav@gmail.com>
2018-09-22 21:01:15 +0300

    piris web application initial commit

---
+++ Piris/lab_1/web_app/Bank_System/Bank_System/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bank_System.Startup))]
+namespace Bank_System
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
